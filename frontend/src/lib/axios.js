b244eeef2cb88683ab2e1a7e0294cc8e1d59cdbf
Abubakar Lawan <abubakarlawan671@gmail.com>
2025-12-11 09:00:12 +0100

    prepared for the deployement

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASAE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASAE_URL,
 });
 
 export default api;
